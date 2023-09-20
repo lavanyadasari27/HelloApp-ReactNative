68e37842a873e35b24bddabb6ec4e4617b19ccf3
lavanyadasari <lavanya.dasari@everest.engineering>
2023-09-20 21:55:02 +0530

    Basic app with Hello and name is implemented

--- App.tsx
+++ App.tsx
@@ -1,1 +1,15 @@
-import React from 'react';
+import React from 'react';import {Text, View} from 'react-native';
+
+const App = () => {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}>
+      <Text>Hello, Lavanya!!!</Text>
+    </View>
+  );
+};
+export default App;
